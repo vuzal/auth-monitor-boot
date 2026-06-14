7516b1419c93f6852fbb5d635bede731dc51b221
Vusal Abbasov <vusalabbasovredmi9@gmail.com>
2026-06-14 11:59:54 +0400

    first commit

---
+++ src/main/java/com/vusal/authmonitorboot/AuthMonitorBootApplication.java
@@ -0,0 +1,13 @@
+package com.vusal.authmonitorboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMonitorBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthMonitorBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vusal/authmonitorboot/AuthMonitorBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vusal.authmonitorboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMonitorBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
